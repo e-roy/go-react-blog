e6c4de3293bae6c57cb6291042a32aff4a74302a
e-roy <e-roy@users.noreply.github.com>
2025-09-06 05:39:14 -0400

    fix: change to ssr false

--- frontend/react-router.config.ts
+++ frontend/react-router.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "@react-router/dev/config";
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  ssr: false,
   // Build as static site for deployment
   buildDirectory: "dist",
 } satisfies Config;
